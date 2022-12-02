bd2a32653b713365516a38b22221d7ec31212cf0
BaDxKaRMa <bad87karma@gmail.com>
2022-12-01 22:22:39 -0800

    finished day 2 using rps unicode hacks :)

---
+++ day2/day2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+try:
+    with open("input.txt", "r") as f:
+        input_file = f.read()
+except FileNotFoundError:
+    exit("Input file not found")
+
+SAMPLE_INPUT = ["A Y", "B X", "C Z"]
+
+# Unicode hacks lul
+
+
+def part1(lines):
+    """
+    >>> part1(SAMPLE_INPUT)
+    15
+    """
+    return sum(
+        ((p2 - p1 + 1) % 3) * 3 + p2
+        for (p1, p2) in ((ord(l[0]) - 64, ord(l[2]) - 87) for l in lines)
+    )
+
+
+def part2(lines):
+    """
+    >>> part2(SAMPLE_INPUT)
+    12
+    """
+    return sum(
+        win * 3 + (p1 + win - 2) % 3 + 1
+        for (p1, win) in ((ord(l[0]) - 64, ord(l[2]) - 88) for l in lines)
+    )
+
+
+parts = (part1, part2)
+
+for i, part in enumerate(parts, 1):
+    print(f"Part {i}: {part(input_file.splitlines())}")
