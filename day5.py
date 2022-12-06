7f81654a781ed7068d46298d3aea5b02e2a127af
BaDxKaRMa <bad87karma@gmail.com>
2022-12-06 02:19:12 -0500

    fix day5 test

--- day5.py
+++ day5.py
@@ -19,6 +19,10 @@ stacks = defaultdict(list)
 
 
 def run(lines, part):
+    """
+    >>> run(SAMPLE_INPUT, 1)
+    defaultdict(<class 'list'>, {4: ['L', 'D', 'B', 'S'], 5: ['J', 'F'], 7: ['L', 'J', 'H', 'W', 'H', 'Z', 'F', 'T'], 3: ['T', 'N', 'B', 'F'], 8: ['B', 'C'], 6: ['R', 'H'], 2: ['L', 'M', 'H', 'Q', 'J', 'J', 'Q', 'D', 'T', 'T', 'F', 'Q', 'T', 'B', 'N', 'P'], 1: ['G', 'N', 'B', 'R', 'S', 'N', 'J', 'W', 'L', 'R', 'D', 'W', 'S', 'M', 'M', 'N', 'D', 'C', 'M'], 0: ['R', 'H', 'V', 'N', 'Z']})
+    """
     for line in lines:
         if "[" in line:
             for i in range(1, len(line) - 1, 4):
@@ -32,6 +36,11 @@ def run(lines, part):
                 stacks[c - 1] = stacks[b - 1][:a] + stacks[c - 1]
             stacks[b - 1] = stacks[b - 1][a:]
     return stacks
+
+
+def test_run():
+    run(SAMPLE_INPUT, 1)
+    assert "".join(stacks[i][0] for i in range(len(stacks))) == "RGLTLJRLB"
 
 
 try:
